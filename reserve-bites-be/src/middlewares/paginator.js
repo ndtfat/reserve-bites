302ab34b54cbb06e253701435be208e3796e2606
Phat Nguyen <tony@sharing365.io>
2024-02-27 16:22:18 +0700

    Reset pageSize

--- reserve-bites-be/src/middlewares/paginator.js
+++ reserve-bites-be/src/middlewares/paginator.js
@@ -3,7 +3,7 @@ export default async (req, res, next) => {
     let { page, sortBy } = req.query;
     page = Number(page) || 1;
     sortBy = (sortBy === 'newest' ? 'desc' : 'asc') || 'desc';
-    const pageSize = 1;
+    const pageSize = 10;
     const offset = (page - 1) * pageSize;
 
     const paginator = { page, pageSize, offset, sortBy };
